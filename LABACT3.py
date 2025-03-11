f9658db6e277f6bf4102ea408a189d032a29a5ef
cyanxianv <christianjbvinluan@gmail.com>
2025-03-11 22:17:48 +0800

    Add files via upload

---
+++ LABACT3.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you?"))
+if age >=100:
+	print("Ho is you ancient?!")
+elif age >= 65:
+	print("You're a Senior")
+elif age >= 20:
+	print("You're an Adult")
+elif age >= 13:
+	print("You're a Teenager")
+elif age >= 0:
+	print("You're a Child")
+else:
+	print("Invalid Age")					
